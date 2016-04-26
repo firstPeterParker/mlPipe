0740fdfd98cca457bec57c6100d8d7dfc77da6cc
Heith Seewald <heiths@gmail.com>
2016-04-26 13:18:55 -0400

    Update maFileFinder.py

--- maFileFinder.py
+++ maFileFinder.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+
 import os
+import fnmatch
 
-dirs = os.listdir('/users/matthew/desktop/')
 
-maFls = []
+def find_by_extension(ext, search_path=None, verbose=False):
+    if search_path is None:
+        search_path = os.path.expanduser("~")
 
-for dir in dirs:
-    if dir.endswith('.ma'):
-        maFls.append(dir)
+    matches = []
+    for root, dir_names, file_names in os.walk(search_path):
+        for filename in fnmatch.filter(file_names, '*.{}'.format(ext)):
+            matches.append(os.path.join(root, filename))
 
-print maFls
+    if verbose:
+        print(matches)
+    print("Found {} matching files.".format(len(matches)))
+
+    return matches
+
+
+if __name__ == '__main__':
+    find_by_extension("ma", verbose=True)
